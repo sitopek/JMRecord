eeb960e2c807bc3ed3fe061086f15fdc0dcf91b0
topek <24753204+sitopek@users.noreply.github.com>
2018-07-18 11:00:11 +0800

    Update membersite_config.php

--- pages/RegistrationForm-master/include/membersite_config.php
+++ pages/RegistrationForm-master/include/membersite_config.php
@@ -9,14 +9,15 @@ $fgmembersite->SetWebsiteName('http://172.168.3.6:8080');
 //Provide the email address where you want to get notifications
 $fgmembersite->SetAdminEmail('heckcrack3r@gmail.com');
 
+
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$fgmembersite->InitDB(/*hostname*/'http://172.168.3.6:8080',
-                      /*username*/'root',
-                      /*password*/'',
-                      /*database name*/'jmleave',
+$fgmembersite->InitDB(/*hostname*/'tcp:jasamerin-sv.database.windows.net',
+                      /*username*/'shairol',
+                      /*password*/'Jasamerin1',
+                      /*database name*/'jasamerin-db',
                       /*table name*/'fgusers3');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
